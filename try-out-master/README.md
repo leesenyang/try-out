20174d8741d5be9ad5dcd28877fe5ddf008aab5d
leesenyang <56668011+leesenyang@users.noreply.github.com>
2020-04-01 13:41:32 +0800

    Add files via upload

---
+++ try-out-master/README.md
@@ -0,0 +1,7 @@
+# try-out
+var test = "hi";
+function newFunction(){
+    var hello = "hello"
+
+}
+console.log(hello);
---
+++ try-out-master/index.js
@@ -0,0 +1,6 @@
+function calling()
+{
+    var x = "hey how was your day?";
+    document.getElementById("yeet").innerHTML = x;
+}
+
